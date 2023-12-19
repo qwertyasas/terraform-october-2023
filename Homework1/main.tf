0620e1c8f28d37be215ec5471445c1a425bacf7e
qwertyasas <ayzhigit.02@gmail.com>
2023-12-19 20:30:11 +0000

    Homework1 test

---
+++ Homework1/main.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "lb1" {
+  name = "Aizhigit"
+}
+
+resource "aws_iam_user" "lb2" {
+  name = "Kaizen"
+}
+
+resource "aws_iam_user" "lb3" {
+  name = "Hello"
+}
+
+resource "aws_iam_user" "lb4" {
+  name = "World"
+}
