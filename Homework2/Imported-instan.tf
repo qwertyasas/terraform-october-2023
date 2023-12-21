061873e4f4be2a58aae49dad3822735b8e0074fd
qwertyasas <45683836+qwertyasas@users.noreply.github.com>
2023-12-21 17:57:02 -0500

    Update Imported-instan.tf

--- Homework2/Imported-instan.tf
+++ Homework2/Imported-instan.tf
@@ -6,3 +6,4 @@ resource "aws_security_group" "ad"{
 }
 # command to import instance --- terraform import aws_instance.as i-0d0322e24eafca049(id of instance)
 # command to import security group --- terraform import aws_security_group.ad sg-0687d36827fe2a570(id of security group)
+# in order to monupilate them we need to specify ami and name for them
