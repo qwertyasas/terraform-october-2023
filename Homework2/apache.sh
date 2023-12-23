448c31b4d0d325d3878e71e9fa6550e43de38d28
qwertyasas <45683836+qwertyasas@users.noreply.github.com>
2023-12-22 23:55:55 -0500

    Update apache.sh

--- Homework2/apache.sh
+++ Homework2/apache.sh
@@ -1,4 +1,3 @@
-sudo apt update
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
